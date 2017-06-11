bb11e5b313a3c8068cc5d9b2d52000a1a3cc9373
Lindsey Erickson <lindseyjerickson@gmail.com>
2017-06-11 11:31:25 -0500

    Create ui.R

---
+++ ui.R
@@ -0,0 +1,21 @@
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Predict Number of Shared Bikes Used from Temperature"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("sliderTemp", "What is the temperature in Celcius?", 0, 1, value = 0.4),
+      checkboxInput("showModel1", "Show/Hide Model", value = TRUE),
+      submitButton("Submit")
+    ),
+  
+  mainPanel(
+    plotOutput("plot1"),
+    h3("Predicting number of shared bikes"),
+    textOutput("pred1")
+  )
+)
+))
